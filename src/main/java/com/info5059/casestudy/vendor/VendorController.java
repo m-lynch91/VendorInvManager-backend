71660a2eabdd90d35e03e4193c4adb332e50c747
Mike Lynch <micheal.j.lynch@gmail.com>
2024-09-24 15:51:04 -0400

    started lab 5

--- src/main/java/com/info5059/casestudy/vendor/VendorController.java
+++ src/main/java/com/info5059/casestudy/vendor/VendorController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -27,6 +29,17 @@ public class VendorController {
     public ResponseEntity<Vendor> updateOne(@RequestBody Vendor vendor) {
         Vendor updatedVendor = vendorRepository.save(vendor);
         return new ResponseEntity<Vendor>(updatedVendor, HttpStatus.OK);
+    }
+
+    @PostMapping("/api/vendors")
+    public ResponseEntity<Vendor> addOne(@RequestBody Vendor vendor) {
+        Vendor newVendor = vendorRepository.save(vendor);
+        return new ResponseEntity<Vendor>(newVendor, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/api/vendors/{id}")
+    public ResponseEntity<Integer> deleteOne(@PathVariable long id) {
+        return new ResponseEntity<Integer>(vendorRepository.deleteOne(id), HttpStatus.OK);
     }
     
 }
